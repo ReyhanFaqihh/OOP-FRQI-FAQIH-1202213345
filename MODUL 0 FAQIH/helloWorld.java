0335c21156f587ceb07492e5ea9bc0c5f4a4ec04
ReyhanFaqihh <reyhanel1316@gmail.com>
2022-12-04 18:52:35 +0700

    PRAKTIKUM

--- MODUL 0 FAQIH/helloWorld.java
+++ MODUL 0 FAQIH/helloWorld.java
@@ -1,5 +1,5 @@
 public class helloWorld {
     public static void main(String[] arg) throws Exception {
-        System.out.println("Hello World !!");
+        System.out.println("Hello World :)");
     }
 }
